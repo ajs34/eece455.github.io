050099ae6e8a16b66ebba5e0c3c399ab02c543f1
Mohamad Abou Harb <mohabouharb@gmail.com>
2020-11-11 22:59:20 +0200

    added affine cipher

--- js/classic.js
+++ js/classic.js
@@ -1,7 +1,7 @@
 const caesarCipher = (str, amount) => {
   if (amount < 0) 
     return caesarCipher(str, amount + 26);
-  var output = "";
+  let output = "";
   for (var i = 0; i < str.length; i++) {
     var c = str[i];
     if (c.match(/[a-z]/i)) {
@@ -17,11 +17,54 @@ const caesarCipher = (str, amount) => {
   }
   return output;
 };
-
-const gcd = () => {
+const gcd = (a,b) => {
      if (b)
         return gcd(b, a % b);
      else
         return Math.abs(a);
 };
 
+const affineCipherEncrypt = (plaintext,a,b)=>{
+    let cipher = '';
+    let value = 0;
+    plaintext.split('').forEach((char) => {
+      if(char.match(/[a-z]/i)){
+          let offset = (char.charCodeAt(0) >= 97) ? 'a':'A';
+          value = ((a*(char.charCodeAt(0)-offset.charCodeAt(0)) + b)%26) + offset.charCodeAt(0);
+          cipher += String.fromCharCode(value);
+      }
+      else{
+        cipher += char;
+      }
+    });
+    return cipher;
+}
+
+const affineCipherDecrypt = (cipher,a,b)=>{
+    let plaintext = '';
+    let aInverse = 0;
+    let value = 0;
+    for(var i = 0;i < a;i++){
+      if(a*i % 26 == 1){
+          aInverse = i;
+          break;
+      }
+    }
+    cipher.split('').forEach((char)=>{
+      if(char.match(/[a-z]/i)){
+          let offset = (char.charCodeAt(0) >= 97) ? 'a':'A';
+          let x = char.charCodeAt(0) - offset.charCodeAt(0)
+          console.log(x)
+          value = mod(aInverse*(x - b),26)
+          plaintext+=String.fromCharCode(value+offset.charCodeAt(0))
+      }
+      else{
+        plaintext += char;
+      }
+    })
+    return plaintext;
+}
+function mod(n, m) {
+    var remain = n % m;
+    return Math.floor(remain >= 0 ? remain : remain + m);
+};
